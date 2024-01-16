b8d738e5d3cba2d6f9bbaabec28888a25ad25b6c
nandaconte <fernanda.accsilva@senacsp.edu.br>
2024-01-16 17:45:58 -0300

    home atualizada - carrossel (desktop não funcionando)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Menu hamburguer - mobile
+
 let menu = document.querySelector("#menu-icon");
 
 let navBar = document.querySelector("#nav-bar");
@@ -9,5 +11,23 @@ menu.onclick = () =>{
 window.onscroll = () =>{
     navBar.classList.remove("active");
 }
+// Fim Menu hamburguer - mobile
 
+// Carrossel
+let contador = 1
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage()
+},4000);
+
+function nextImage(){
+    contador++;
+    if(contador > 4){
+        contador=1;
+    }
+    document.getElementById("radio"+contador).checked = true;
+}
+// Fim Carrossel
 
